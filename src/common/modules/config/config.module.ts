cfd89c71b283eb086f6b145b0576a49456ebdab0
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-14 14:43:45 -0500

    feat(config.module.ts): add configuration setup for ConfigModule
    Include configuration setup in ConfigModule with the following:
    - Import NestConfigModule and configure it with isGlobal, load, validate, and cache options.
    - Load configuration from 'config.config.ts'.
    - Add validation from 'validate.validation.ts'.

--- src/common/modules/config/config.module.ts
+++ src/common/modules/config/config.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import config from './envs/configs/config.config';
+import { validate } from './envs/validations/validate.validation';
 
-@Module({})
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+      validate,
+      cache: true,
+    }),
+  ],
+})
 export class ConfigModule {}
