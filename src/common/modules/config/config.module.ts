25097f2259e46b5c896e47775d108af077dee360
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-14 15:13:28 -0500

    feat(config.module.ts): add OpenAI configuration to NestConfigModule
    import and load OpenAI configuration

--- src/common/modules/config/config.module.ts
+++ src/common/modules/config/config.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import config from './envs/configs/config.config';
+import openai from './envs/configs/openai.config';
 import { validate } from './envs/validations/validate.validation';
 
 @Module({
   imports: [
     NestConfigModule.forRoot({
       isGlobal: true,
-      load: [config],
+      load: [config, openai],
       validate,
       cache: true,
     }),
