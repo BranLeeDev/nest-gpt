6a392b3a64642c8fca667b46ccdbcf8ab8d83683
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-14 18:38:25 -0500

    feat(openai.service.ts): initialize OpenAI instance with secret key

--- src/ai/services/openai.service.ts
+++ src/ai/services/openai.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { OpenAI } from 'openai';
+import openai from '@configs/openai.config';
 
 @Injectable()
-export class OpenaiService {}
+export class OpenaiService {
+  constructor(
+    @Inject(openai.KEY)
+    private readonly configService: ConfigType<typeof openai>,
+  ) {}
+
+  readonly openAi = new OpenAI({
+    apiKey: this.configService.secretKey,
+  });
+}
