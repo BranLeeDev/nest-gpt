ad8bc4d9ce1b66ebb857fd6ebc1ae20974b591c7
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-14 18:42:25 -0500

    feat: generate 'gpt' module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { AiModule } from './ai/ai.module';
+import { GptModule } from './gpt/gpt.module';
 
 @Module({
-  imports: [CommonModule, AiModule],
+  imports: [CommonModule, AiModule, GptModule],
   controllers: [],
   providers: [],
 })
