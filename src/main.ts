2be3542c1da70d0a927d08e06d50a1529ad0b162
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-14 19:07:27 -0500

    feat(main.ts): add bufferLogs and userLogger with Fastify

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,18 @@ import {
   FastifyAdapter,
 } from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { bufferLogs: true },
   );
   const configService = app.get(ConfigService);
   const PORT = Number(configService.get('PORT'));
+  app.useLogger(app.get(Logger));
   await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
