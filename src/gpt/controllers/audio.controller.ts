755462b9f19c4a9d03928a17b71466674eed3eae
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-15 13:38:26 -0500

    feat(audio): add text to audio functionality

--- src/gpt/controllers/audio.controller.ts
+++ src/gpt/controllers/audio.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
+import type { FastifyReply } from 'fastify';
+import { TextToAudioDto } from '../dtos';
+import { AudioService } from '../services/audio.service';
 
 @Controller('audio')
-export class AudioController {}
+export class AudioController {
+  constructor(private readonly audioService: AudioService) {}
+
+  @Post('text-to-audio')
+  @HttpCode(HttpStatus.OK)
+  async textToAudio(
+    @Body() textToAudioDto: TextToAudioDto,
+    @Res() res: FastifyReply,
+  ) {
+    const buffer = await this.audioService.textToAudio(textToAudioDto);
+    res.type('audio/mp3').send(buffer);
+  }
+}
--- src/gpt/services/audio.service.ts
+++ src/gpt/services/audio.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { join, resolve } from 'node:path';
+import { randomUUID } from 'node:crypto';
+import { mkdir, writeFile } from 'node:fs/promises';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
+import { TextToAudioDto } from '../dtos';
+import { OpenaiService } from '../../ai/services/openai.service';
 
 @Injectable()
-export class AudioService {}
+export class AudioService {
+  private readonly logger = new Logger(AudioService.name);
+
+  constructor(private readonly openaiService: OpenaiService) {}
+
+  async textToAudio(textToAudio: TextToAudioDto) {
+    const { prompt, voice } = textToAudio;
+    this.logger.log(`Generating audio for text: ${prompt}`);
+    const folderPath = resolve(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'generated',
+      'audios',
+    );
+    const speechFile = join(folderPath, `${randomUUID()}.mp3`);
+    await mkdir(folderPath, { recursive: true });
+    try {
+      const mp3 = await this.openaiService.openAi.audio.speech.create({
+        model: 'tts-1',
+        voice,
+        input: prompt,
+        response_format: 'mp3',
+      });
+      const buffer = Buffer.from(await mp3.arrayBuffer());
+      await writeFile(speechFile, buffer);
+      this.logger.log(`Audio saved to file: ${speechFile}`);
+      return buffer;
+    } catch (error) {
+      this.logger.error({
+        message: error.message,
+        error: error.type,
+        statusCode: error.status,
+      });
+      throw new HttpException(error.message, error.status);
+    }
+  }
+}
