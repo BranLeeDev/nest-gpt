3ef778c7e54b40bc7af629fef617eec9a37cf4e2
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-14 19:16:44 -0500

    feat(logger.module.ts): switch to 'forRootAsync' and configure 'pino-pretty' for development

--- src/common/modules/logger/logger.module.ts
+++ src/common/modules/logger/logger.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { LoggerModule as NestLoggerModule } from 'nestjs-pino';
+import config from '@configs/config.config';
 
 @Module({
-  imports: [NestLoggerModule.forRoot()],
+  imports: [
+    NestLoggerModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { isProd } = configService;
+        return {
+          pinoHttp: {
+            transport: isProd
+              ? undefined
+              : {
+                  target: 'pino-pretty',
+                },
+          },
+        };
+      },
+    }),
+  ],
 })
 export class LoggerModule {}
