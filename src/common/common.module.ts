a72bba4ad8b1fe06fbc8f75aa0f4132d2a36c20c
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-14 19:00:38 -0500

    feat: generate 'logger' module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './modules/config/config.module';
+import { LoggerModule } from './modules/logger/logger.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, LoggerModule],
 })
 export class CommonModule {}
