9b04f59094b8af70aba4f5d1239779a4865c004c
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-15 12:56:42 -0500

    feat(gpt): generate 'audio' service and controller

--- src/gpt/gpt.module.ts
+++ src/gpt/gpt.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TextController } from './controllers/text.controller';
 import { TextService } from './services/text.service';
+import { AudioController } from './controllers/audio.controller';
+import { AudioService } from './services/audio.service';
 
 @Module({
-  controllers: [TextController],
-  providers: [TextService],
+  controllers: [TextController, AudioController],
+  providers: [TextService, AudioService],
 })
 export class GptModule {}
