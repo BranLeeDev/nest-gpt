068fc9d397262053e47f392455b31ea8372dbb28
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-15 12:09:22 -0500

    feat(gpt): add language enum

---
+++ src/gpt/models/enums/lang.enum.ts
@@ -0,0 +1,12 @@
+export enum Lang {
+  ENGLISH = 'Inglés',
+  MANDARIN_CHINESE = 'Chino Mandarín',
+  HINDI = 'Hindi',
+  SPANISH = 'Español',
+  FRENCH = 'Francés',
+  ARABIC = 'Árabe',
+  BENGALI = 'Bengalí',
+  RUSSIAN = 'Ruso',
+  PORTUGUESE = 'Portugués',
+  INDONESIAN = 'Indonesio',
+}
