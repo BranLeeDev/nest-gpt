10a492002d661b1e6ad0001ad9ff44917b39903c
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-16 08:09:32 -0500

    feat(openai.config.ts): add 'maxFileSize' configuration

--- src/common/modules/config/envs/configs/openai.config.ts
+++ src/common/modules/config/envs/configs/openai.config.ts
@@ -3,5 +3,6 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('openai', () => {
   return {
     secretKey: process.env.OPENAI_SECRET_KEY,
+    maxFileSize: process.env.MAX_FILE_SIZE,
   };
 });
