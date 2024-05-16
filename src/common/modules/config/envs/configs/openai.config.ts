f41bc30239dbb4063fc857b0e8ba277a07492fb9
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-16 08:20:40 -0500

    feat(openai.config.ts): add Number conversion to maxFileSize

--- src/common/modules/config/envs/configs/openai.config.ts
+++ src/common/modules/config/envs/configs/openai.config.ts
@@ -3,6 +3,6 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('openai', () => {
   return {
     secretKey: process.env.OPENAI_SECRET_KEY,
-    maxFileSize: process.env.MAX_FILE_SIZE,
+    maxFileSize: Number(process.env.MAX_FILE_SIZE),
   };
 });
