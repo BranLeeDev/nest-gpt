863f93a9e332ab9202f46f59796f3663ecb7aa62
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-16 15:12:07 -0500

    feat(gpt): generate 'image' controller and service

--- src/gpt/gpt.module.ts
+++ src/gpt/gpt.module.ts
@@ -3,9 +3,11 @@ import { TextController } from './controllers/text.controller';
 import { TextService } from './services/text.service';
 import { AudioController } from './controllers/audio.controller';
 import { AudioService } from './services/audio.service';
+import { ImageService } from './services/image.service';
+import { ImageController } from './controllers/image.controller';
 
 @Module({
-  controllers: [TextController, AudioController],
-  providers: [TextService, AudioService],
+  controllers: [TextController, AudioController, ImageController],
+  providers: [TextService, AudioService, ImageService],
 })
 export class GptModule {}
