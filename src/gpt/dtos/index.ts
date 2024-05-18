f6134ac2caf1819d1834b2989be33c9f6ef961ae
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-18 15:28:52 -0500

    feat(gpt): export 'image-variation.dto.ts' file

--- src/gpt/dtos/index.ts
+++ src/gpt/dtos/index.ts
@@ -3,3 +3,4 @@ export * from './text/argumentative.dto';
 export * from './text/translate.dto';
 export * from './audio/text-to-audio.dto';
 export * from './image/image-generation.dto';
+export * from './image/image-variation.dto';
