7620e449a3ea06f7de2ba9d12d035a9d1c663ae7
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-18 20:23:26 -0500

    feat(images.util.ts): check if image exists before downloading in downloadImage function

--- src/common/utils/images.util.ts
+++ src/common/utils/images.util.ts
@@ -1,12 +1,37 @@
 import { Buffer } from 'node:buffer';
 import { InternalServerErrorException } from '@nestjs/common';
-import { saveFileToGenerated } from './files.util';
+import {
+  checkIfFileExists,
+  getFilePath,
+  saveFileToGenerated,
+} from './files.util';
 
 export async function downloadImage(url: string, imageType: string) {
+  const imageExists = getFilePath(url);
+  if (imageExists) {
+    const fileId = imageExists.name;
+    const { filePath: imageFilePath } = checkIfFileExists(
+      'images',
+      fileId,
+      'png',
+    );
+    return {
+      imageFilePath,
+      fileId,
+    };
+  }
   const response = await fetch(url);
   if (!response.ok) {
     throw new InternalServerErrorException('Download image was not possible');
   }
   const buffer = Buffer.from(await response.arrayBuffer());
-  await saveFileToGenerated(buffer, 'images', imageType);
+  const { filePath: imageFilePath, fileId } = await saveFileToGenerated(
+    buffer,
+    'images',
+    imageType,
+  );
+  return {
+    imageFilePath,
+    fileId,
+  };
 }
