2094675835266dc157cd20c46a6cdba07bb179ef
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-18 20:38:18 -0500

    feat(config.config.ts): add serverUrl to configuration

--- src/common/modules/config/envs/configs/config.config.ts
+++ src/common/modules/config/envs/configs/config.config.ts
@@ -5,5 +5,6 @@ export default registerAs('config', () => {
     env: process.env.NODE_ENV,
     isProd: process.env.NODE_ENV === 'production',
     port: process.env.PORT,
+    serverUrl: process.env.SERVER_URL,
   };
 });
