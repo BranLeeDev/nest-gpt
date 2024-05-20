c375b8db3f6c947d86b214ae47685b3666a33321
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-20 15:37:39 -0500

    feat(assistants.controller.ts): remove 'user-question' endpoint

--- src/assistants/controllers/assistants.controller.ts
+++ src/assistants/controllers/assistants.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AssistantsService } from '../services/assistants.service';
-import { CreateMessageDto } from '../dtos';
 
 @Controller('assistants')
 export class AssistantsController {
@@ -9,12 +8,6 @@ export class AssistantsController {
   @Post('create-thread')
   async createThread() {
     const res = await this.assistantsService.createThread();
-    return res;
-  }
-
-  @Post('user-question')
-  async userQuestion(@Body() createMessageDto: CreateMessageDto) {
-    const res = await this.assistantsService.createMessage(createMessageDto);
     return res;
   }
 }
