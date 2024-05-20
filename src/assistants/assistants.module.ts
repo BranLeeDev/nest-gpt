670a728795540319ab55bb5da46603fc6bbb938d
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-20 12:52:15 -0500

    feat(assistants): generate 'assistants' controller

--- src/assistants/assistants.module.ts
+++ src/assistants/assistants.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SamAssistantController } from './controllers/sam-assistant.controller';
 import { AssistantsService } from './services/assistants.service';
+import { AssistantsController } from './controllers/assistants.controller';
 
 @Module({
-  controllers: [SamAssistantController],
+  controllers: [SamAssistantController, AssistantsController],
   providers: [AssistantsService],
 })
 export class AssistantsModule {}
