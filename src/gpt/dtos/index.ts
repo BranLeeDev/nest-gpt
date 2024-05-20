cd2384b444d57b4c7d4c0bf97662a05504113648
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-19 20:39:54 -0500

    feat(gpt): export 'image-masking.dto.ts' file

--- src/gpt/dtos/index.ts
+++ src/gpt/dtos/index.ts
@@ -4,3 +4,4 @@ export * from './text/translate.dto';
 export * from './audio/text-to-audio.dto';
 export * from './image/image-generation.dto';
 export * from './image/image-variation.dto';
+export * from './image/image-masking.dto';
