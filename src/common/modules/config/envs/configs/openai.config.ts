7f546a12b3f2809623af998bb9f1e10a057a758c
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-20 18:29:52 -0500

    feat(openai.config.ts): add 'samAssistantId' configuration

--- src/common/modules/config/envs/configs/openai.config.ts
+++ src/common/modules/config/envs/configs/openai.config.ts
@@ -4,5 +4,8 @@ export default registerAs('openai', () => {
   return {
     secretKey: process.env.OPENAI_SECRET_KEY,
     maxFileSize: Number(process.env.MAX_FILE_SIZE),
+    assistants: {
+      samAssistantId: process.env.SAM_ASSISTANT_ID,
+    },
   };
 });
