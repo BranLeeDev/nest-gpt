995a65ca86698202173bb7a639ca8de8bb8f9019
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-20 12:05:02 -0500

    feat: generate 'assistants' module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { AiModule } from './ai/ai.module';
 import { GptModule } from './gpt/gpt.module';
+import { AssistantsModule } from './assistants/assistants.module';
 
 @Module({
-  imports: [CommonModule, AiModule, GptModule],
+  imports: [CommonModule, AiModule, GptModule, AssistantsModule],
   controllers: [],
   providers: [],
 })
