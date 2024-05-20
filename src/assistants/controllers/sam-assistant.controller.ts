6815938de11f654d79cca79e0aa850eea51ea7af
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-20 18:40:41 -0500

    feat(sam-assistant): add user question functionality

--- src/assistants/controllers/sam-assistant.controller.ts
+++ src/assistants/controllers/sam-assistant.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateMessageDto } from '../dtos';
+import { SamAssistantService } from '../services/sam-assistant.service';
 
 @Controller('sam-assistant')
-export class SamAssistantController {}
+export class SamAssistantController {
+  constructor(private readonly samAssistant: SamAssistantService) {}
+
+  @Post('user-question')
+  async userQuestion(@Body() createMessageDto: CreateMessageDto) {
+    const res = await this.samAssistant.userQuestion(createMessageDto);
+    return res;
+  }
+}
--- src/assistants/services/sam-assistant.service.ts
+++ src/assistants/services/sam-assistant.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { AssistantsService } from './assistants.service';
+import { CreateMessageDto } from '../dtos';
+import openai from '@configs/openai.config';
 
 @Injectable()
-export class SamAssistantService {}
+export class SamAssistantService {
+  constructor(
+    private readonly assistantsService: AssistantsService,
+    @Inject(openai.KEY)
+    private readonly configService: ConfigType<typeof openai>,
+  ) {}
+
+  async userQuestion(createMessageDto: CreateMessageDto) {
+    const { samAssistantId } = this.configService.assistants;
+    const { threadId, question } = createMessageDto;
+    await this.assistantsService.createMessage(threadId, question);
+    const run = await this.assistantsService.createRun(
+      threadId,
+      samAssistantId!,
+    );
+    await this.assistantsService.checkRunCompleteStatus(run.id, threadId);
+    const messages = await this.assistantsService.getMessagesList(threadId);
+    return messages;
+  }
+}
