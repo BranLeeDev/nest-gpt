b6c78f111c95800205cc14e2725ce68505bb942b
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-20 13:55:19 -0500

    feat(assistants.service.ts): add 'createMessage' method with configuration

--- src/assistants/services/assistants.service.ts
+++ src/assistants/services/assistants.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { OpenaiService } from '../../ai/services/openai.service';
+import { CreateMessageDto } from '../dtos';
 
 @Injectable()
 export class AssistantsService {
@@ -8,5 +9,15 @@ export class AssistantsService {
   async createThread() {
     const thread = await this.openaiService.openAi.beta.threads.create();
     return thread;
+  }
+
+  async createMessage(createMessageDto: CreateMessageDto) {
+    const { threadId, question } = createMessageDto;
+    const message =
+      await this.openaiService.openAi.beta.threads.messages.create(threadId, {
+        role: 'user',
+        content: question,
+      });
+    return message;
   }
 }
