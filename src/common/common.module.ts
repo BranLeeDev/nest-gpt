3ed7cc2b11827fac1ce9d9bfb3e2eea547927f12
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-21 16:03:41 -0500

    feat(common): generate 'throttler' module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './modules/config/config.module';
 import { LoggerModule } from './modules/logger/logger.module';
+import { ThrottlerModule } from './modules/throttler/throttler.module';
 
 @Module({
-  imports: [ConfigModule, LoggerModule],
+  imports: [ConfigModule, LoggerModule, ThrottlerModule],
 })
 export class CommonModule {}
