f1d4473ff659629f92be4aff7dcba02fee05d9fe
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-20 19:33:29 -0500

    feat(sam-assistant.service.ts): implement try-catch and NestJS logger

--- src/assistants/services/sam-assistant.service.ts
+++ src/assistants/services/sam-assistant.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { AssistantsService } from './assistants.service';
 import { CreateMessageDto } from '../dtos';
@@ -6,6 +6,8 @@ import openai from '@configs/openai.config';
 
 @Injectable()
 export class SamAssistantService {
+  private readonly logger = new Logger(SamAssistantService.name);
+
   constructor(
     private readonly assistantsService: AssistantsService,
     @Inject(openai.KEY)
@@ -15,13 +17,24 @@ export class SamAssistantService {
   async userQuestion(createMessageDto: CreateMessageDto) {
     const { samAssistantId } = this.configService.assistants;
     const { threadId, question } = createMessageDto;
-    await this.assistantsService.createMessage(threadId, question);
-    const run = await this.assistantsService.createRun(
-      threadId,
-      samAssistantId!,
-    );
-    await this.assistantsService.checkRunCompleteStatus(threadId, run.id);
-    const messages = await this.assistantsService.getMessagesList(threadId);
-    return messages;
+    try {
+      this.logger.log(`User question received: ${question}`);
+      await this.assistantsService.createMessage(threadId, question);
+      const run = await this.assistantsService.createRun(
+        threadId,
+        samAssistantId!,
+      );
+      await this.assistantsService.checkRunCompleteStatus(threadId, run.id);
+      const messages = await this.assistantsService.getMessagesList(threadId);
+      this.logger.log(`Processed user question for thread ${threadId}`);
+      return messages;
+    } catch (error) {
+      this.logger.error({
+        message: error.message,
+        error: error.type,
+        statusCode: error.status,
+      });
+      throw new HttpException(error.message, error.status);
+    }
   }
 }
