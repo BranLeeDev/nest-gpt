4520a383d9b2e04f755ad5bcdb5bb650a4a45209
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-20 19:27:56 -0500

    feat(assistants.service.ts): implement try-catch and NestJS logger

--- src/assistants/services/assistants.service.ts
+++ src/assistants/services/assistants.service.ts
@@ -1,60 +1,138 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
 import { OpenaiService } from '../../ai/services/openai.service';
 
 @Injectable()
 export class AssistantsService {
+  private readonly logger = new Logger(AssistantsService.name);
+
   constructor(private readonly openaiService: OpenaiService) {}
 
   async createThread() {
-    const thread = await this.openaiService.openAi.beta.threads.create();
-    return thread;
+    try {
+      this.logger.log('Starting createThread method');
+      const thread = await this.openaiService.openAi.beta.threads.create();
+      this.logger.log('Successfully created a thread');
+      return thread;
+    } catch (error) {
+      this.logger.error({
+        message: error.message,
+        error: error.type,
+        statusCode: error.status,
+      });
+      throw new HttpException(error.message, error.status);
+    }
   }
 
   async getMessagesList(threadId: string) {
-    const messagesList =
-      await this.openaiService.openAi.beta.threads.messages.list(threadId);
-    const messages = messagesList.data.map((message) => ({
-      role: message.role,
-      content: message.content.map((content) => (content as any).text.value),
-    }));
-    return messages.reverse();
+    try {
+      this.logger.log(
+        `Starting getMessagesList method for threadId: ${threadId}`,
+      );
+      const messagesList =
+        await this.openaiService.openAi.beta.threads.messages.list(threadId);
+      this.logger.debug('Raw messages list retrieved', messagesList);
+      const messages = messagesList.data.map((message) => ({
+        role: message.role,
+        content: message.content.map((content) => (content as any).text.value),
+      }));
+      this.logger.debug('Formatted messages list', messages);
+      this.logger.log(
+        `Successfully retrieved messages for threadId: ${threadId}`,
+      );
+      return messages.reverse();
+    } catch (error) {
+      this.logger.error({
+        message: error.message,
+        error: error.type,
+        statusCode: error.status,
+      });
+      throw new HttpException(error.message, error.status);
+    }
   }
 
   async checkRunCompleteStatus(threadId: string, runId: string) {
-    while (true) {
-      const runStatus = await this.retrieveRunStatus(threadId, runId);
-      if (runStatus.status === 'completed') {
-        return runStatus.status;
+    try {
+      this.logger.log(
+        `Starting checkRunCompleteStatus method for threadId: ${threadId} and runId: ${runId}`,
+      );
+      while (true) {
+        const runStatus = await this.retrieveRunStatus(threadId, runId);
+        if (runStatus.status === 'completed') {
+          this.logger.log(
+            `Run completed for threadId: ${threadId}, runId: ${runId}`,
+          );
+          return runStatus.status;
+        }
+        await new Promise((resolve) => setTimeout(resolve, 1500));
       }
-      await new Promise((resolve) => setTimeout(resolve, 1500));
+    } catch (error) {
+      this.logger.error({
+        message: error.message,
+        error: error.type,
+        statusCode: error.status,
+      });
+      throw new HttpException(error.message, error.status);
     }
   }
 
   private async retrieveRunStatus(threadId: string, runId: string) {
-    const runStatus =
-      await this.openaiService.openAi.beta.threads.runs.retrieve(
-        threadId,
-        runId,
-      );
-    return runStatus;
+    try {
+      const runStatus =
+        await this.openaiService.openAi.beta.threads.runs.retrieve(
+          threadId,
+          runId,
+        );
+      return runStatus;
+    } catch (error) {
+      this.logger.error({
+        message: error.message,
+        error: error.type,
+        statusCode: error.status,
+      });
+      throw new HttpException(error.message, error.status);
+    }
   }
 
   async createRun(threadId: string, assistantId: string) {
-    const run = await this.openaiService.openAi.beta.threads.runs.create(
-      threadId,
-      {
-        assistant_id: assistantId,
-      },
-    );
-    return run;
+    try {
+      this.logger.log(
+        `Starting createRun method for threadId: ${threadId} and assistantId: ${assistantId}`,
+      );
+      const run = await this.openaiService.openAi.beta.threads.runs.create(
+        threadId,
+        {
+          assistant_id: assistantId,
+        },
+      );
+      this.logger.log('Run created');
+      return run;
+    } catch (error) {
+      this.logger.error({
+        message: error.message,
+        error: error.type,
+        statusCode: error.status,
+      });
+      throw new HttpException(error.message, error.status);
+    }
   }
 
   async createMessage(threadId: string, question: string) {
-    const message =
-      await this.openaiService.openAi.beta.threads.messages.create(threadId, {
-        role: 'user',
-        content: question,
+    try {
+      this.logger.log(`Creating message for thread ${threadId}: ${question}`);
+      const message =
+        await this.openaiService.openAi.beta.threads.messages.create(threadId, {
+          role: 'user',
+          content: question,
+        });
+      this.logger.log(`Message created successfully for thread ${threadId}`);
+      return message;
+    } catch (error) {
+      this.logger.error({
+        message: error.message,
+        error: error.type,
+        statusCode: error.status,
       });
-    return message;
+      throw new HttpException(error.message, error.status);
+    }
   }
 }
