0368a62c4eaa9218b8a1a3189e3ad4228e48ba14
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-21 16:16:49 -0500

    feat(throttler): integrate throttling configuration

--- src/common/modules/throttler/throttler.module.ts
+++ src/common/modules/throttler/throttler.module.ts
@@ -1,4 +1,32 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigType } from '@nestjs/config';
+import {
+  ThrottlerModule as NestThrottlerModule,
+  ThrottlerGuard,
+} from '@nestjs/throttler';
+import config from '@configs/config.config';
 
-@Module({})
+@Module({
+  imports: [
+    NestThrottlerModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { throttleTtl, throttleLimit } = configService;
+        return [
+          {
+            ttl: throttleTtl * 3600000,
+            limit: throttleLimit,
+          },
+        ];
+      },
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+})
 export class ThrottlerModule {}
