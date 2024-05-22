1688fbd86c68bb2877e98313d44e5b62520689ff
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-21 19:06:28 -0500

    feat(common): generate 'swagger' module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from './modules/config/config.module';
 import { LoggerModule } from './modules/logger/logger.module';
 import { ThrottlerModule } from './modules/throttler/throttler.module';
+import { SwaggerModule } from './modules/swagger/swagger.module';
 
 @Module({
-  imports: [ConfigModule, LoggerModule, ThrottlerModule],
+  imports: [ConfigModule, LoggerModule, ThrottlerModule, SwaggerModule],
 })
 export class CommonModule {}
