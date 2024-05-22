b46374bc4c449af787d9ea8ff868d5af6a91e257
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 13:22:43 -0500

    feat(common): generate 'serve-static' module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,8 +3,15 @@ import { ConfigModule } from './modules/config/config.module';
 import { LoggerModule } from './modules/logger/logger.module';
 import { ThrottlerModule } from './modules/throttler/throttler.module';
 import { SwaggerModule } from './modules/swagger/swagger.module';
+import { ServeStaticModule } from './modules/serve-static/serve-static.module';
 
 @Module({
-  imports: [ConfigModule, LoggerModule, ThrottlerModule, SwaggerModule],
+  imports: [
+    ConfigModule,
+    LoggerModule,
+    ThrottlerModule,
+    SwaggerModule,
+    ServeStaticModule,
+  ],
 })
 export class CommonModule {}
