32df730312d9fc9f80c23dddedf08b827c3de337
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 09:53:59 -0500

    docs(create-message.dto.ts): add documentation with swagger

--- src/assistants/dtos/assistants/create-message.dto.ts
+++ src/assistants/dtos/assistants/create-message.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength } from 'class-validator';
 
 export class CreateMessageDto {
+  @ApiProperty({
+    description: 'The ID of the thread to which the message belongs',
+    minLength: 7,
+  })
   @IsString()
   @MinLength(7)
   readonly threadId: string;
 
+  @ApiProperty({
+    description: 'The question to be answered in the message',
+    minLength: 3,
+    example: 'What is the valid delivery time for a product?',
+  })
   @IsString()
   @MinLength(3)
   readonly question: string;
