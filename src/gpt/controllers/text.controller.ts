31b09d6b75e68f99cbd806b56d3c31cdd4559c2a
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 10:11:50 -0500

    docs(text.controller.ts): add documentation with three response types

--- src/gpt/controllers/text.controller.ts
+++ src/gpt/controllers/text.controller.ts
@@ -7,7 +7,13 @@ import {
   Post,
   Res,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import type { FastifyReply } from 'fastify';
 import { ArgumentativeDto, OrthographyDto, TranslateDto } from '../dtos';
 import { TextService } from '../services/text.service';
@@ -19,18 +25,39 @@ export class TextController {
 
   constructor(private readonly textService: TextService) {}
 
+  @ApiCreatedResponse({ description: 'Orthography check successful' })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many request. PLease try again later',
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   @Post('orthography-check')
   async orthographyCheck(@Body() orthographyDto: OrthographyDto) {
     const res = await this.textService.orthographyCheck(orthographyDto);
     return res;
   }
 
+  @ApiCreatedResponse({ description: 'Argumentative text analysis successful' })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many request. PLease try again later',
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   @Post('pros-cons-argumentative')
   async prosConsArgumentative(@Body() argumentativeDto: ArgumentativeDto) {
     const res = await this.textService.prosConsArgumentative(argumentativeDto);
     return res;
   }
 
+  @ApiOkResponse({
+    description:
+      'Successful response to pros-cons-argumentative-stream request',
+  })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many request. PLease try again later',
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   @Post('pros-cons-argumentative-stream')
   @HttpCode(HttpStatus.OK)
   async prosConsArgumentativeStream(
@@ -47,12 +74,26 @@ export class TextController {
     this.logger.log('Pros-cons argumentative (stream) completed');
   }
 
+  @ApiCreatedResponse({ description: 'Translation successful' })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many request. PLease try again later',
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   @Post('translate')
   async translate(@Body() translateDto: TranslateDto) {
     const res = await this.textService.translate(translateDto);
     return res;
   }
 
+  @ApiOkResponse({
+    description: 'Successful response to translation stream request',
+  })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many request. PLease try again later',
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   @Post('translate-stream')
   @HttpCode(HttpStatus.OK)
   async translateStream(
