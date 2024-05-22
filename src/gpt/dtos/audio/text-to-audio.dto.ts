bf3bb45a7976af7504b72ae2811146058a2ff0b5
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 09:32:16 -0500

    docs(text-to-audio.dto.ts): add documentation with swagger

--- src/gpt/dtos/audio/text-to-audio.dto.ts
+++ src/gpt/dtos/audio/text-to-audio.dto.ts
@@ -1,11 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
 import { Voice } from '../../models';
 
 export class TextToAudioDto {
+  @ApiProperty({
+    description: 'The text to be converted to audio',
+    minLength: 3,
+    example: 'Hello, world!',
+  })
   @IsString()
   @MinLength(3)
   readonly prompt: string;
 
+  @ApiProperty({
+    description: 'The voice to use for the audio conversion',
+    enum: Voice,
+    default: Voice.ALLOY,
+    example: Voice.ECHO,
+  })
   @IsEnum(Voice)
   @IsOptional()
   readonly voice: Voice = Voice.ALLOY;
