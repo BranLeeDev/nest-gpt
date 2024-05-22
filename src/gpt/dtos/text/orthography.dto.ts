67cbc063ef9d3121710b1557ab5a9e4a0875c23d
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 08:53:31 -0500

    docs(orthography.dto.ts): add documentation for 'prompt' and 'maxTokens' attributes

--- src/gpt/dtos/text/orthography.dto.ts
+++ src/gpt/dtos/text/orthography.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsInt,
   IsOptional,
@@ -8,10 +9,22 @@ import {
 } from 'class-validator';
 
 export class OrthographyDto {
+  @ApiProperty({
+    description: 'A prompt asking for orthographic correction or analysis',
+    minLength: 3,
+    example: 'aqui ai traveja duro tdo el dia',
+  })
   @IsString()
   @MinLength(3)
   readonly prompt: string;
 
+  @ApiProperty({
+    description: 'Maximum number of tokens for the response',
+    minimum: 0,
+    maximum: 150,
+    default: 150,
+    example: 100,
+  })
   @IsInt()
   @Min(0)
   @Max(150)
