958d26e7124119ef39210d6d29a369afb14cde31
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 11:11:51 -0500

    docs(assistants.controller.ts): add documentation with swagger

--- src/assistants/controllers/assistants.controller.ts
+++ src/assistants/controllers/assistants.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AssistantsService } from '../services/assistants.service';
 
 @ApiTags('assistants')
@@ -7,6 +13,17 @@ import { AssistantsService } from '../services/assistants.service';
 export class AssistantsController {
   constructor(private readonly assistantsService: AssistantsService) {}
 
+  @ApiCreatedResponse({
+    description: 'Thread created successfully',
+  })
+  @ApiBadRequestResponse({
+    description: 'An error ocurred',
+  })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many request. PLease try again later',
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   @Post('create-thread')
   async createThread() {
     const res = await this.assistantsService.createThread();
