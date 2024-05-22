5cf770ed781b358251085c99460eb2a49f5d7489
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 09:00:04 -0500

    docs(image-generation.dto.ts): add documentation with swagger
    add description, minLength and example properties

--- src/gpt/dtos/image/image-generation.dto.ts
+++ src/gpt/dtos/image/image-generation.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength } from 'class-validator';
 
 export class ImageGenerationDto {
+  @ApiProperty({
+    description: 'A textual description or prompt for generating an image',
+    minLength: 3,
+    example: 'Generate an image of a futuristic city skyline at sunset',
+  })
   @IsString()
   @MinLength(3)
   readonly prompt: string;
