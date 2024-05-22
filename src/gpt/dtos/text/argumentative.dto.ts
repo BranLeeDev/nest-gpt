021e78bb1d1110ed55944e186f317fe7a812a411
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 08:44:24 -0500

    docs(argumentative.dto.ts): add documentation with swagger
    add description, minLength and example

--- src/gpt/dtos/text/argumentative.dto.ts
+++ src/gpt/dtos/text/argumentative.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength } from 'class-validator';
 
 export class ArgumentativeDto {
+  @ApiProperty({
+    description: 'A prompt asking for a comparison or argumentation',
+    minLength: 3,
+    example: 'Which is better, a laptop or a PC?',
+  })
   @IsString()
   @MinLength(3)
   readonly prompt: string;
