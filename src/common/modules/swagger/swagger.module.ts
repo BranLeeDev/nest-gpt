39e6ca9e5495fe6bfff73800042612e9e2615e95
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-21 21:05:58 -0500

    feat(swagger): setup swagger documentation for Nest GPT API

--- src/common/modules/swagger/swagger.module.ts
+++ src/common/modules/swagger/swagger.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  DocumentBuilder,
+  SwaggerModule as NestSwaggerModule,
+} from '@nestjs/swagger';
+import type { RawServerDefault } from 'fastify';
 
 @Module({})
-export class SwaggerModule {}
+export class SwaggerModule {
+  static setupSwagger(app: NestFastifyApplication<RawServerDefault>) {
+    const config = new DocumentBuilder()
+      .setTitle('Nest GPT')
+      .setDescription('Nest GPT: Fast, secure API for OpenAI GPT')
+      .setVersion('1.0')
+      .build();
+    const document = NestSwaggerModule.createDocument(app, config);
+    NestSwaggerModule.setup('docs', app, document);
+  }
+}
