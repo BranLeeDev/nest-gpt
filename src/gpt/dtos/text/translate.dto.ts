4fa54d457d7c02b6b3885f028d691c7ee2cb56a3
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 08:57:41 -0500

    docs(translate.dto.ts): add documentation for 'prompt' and 'lang' attributes

--- src/gpt/dtos/text/translate.dto.ts
+++ src/gpt/dtos/text/translate.dto.ts
@@ -1,11 +1,22 @@
 import { IsEnum, IsString, MinLength } from 'class-validator';
 import { Lang } from '../../models';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class TranslateDto {
+  @ApiProperty({
+    description: 'The text prompt to be translated',
+    minLength: 3,
+    example: 'Hello, how are you?',
+  })
   @IsString()
   @MinLength(3)
   readonly prompt: string;
 
+  @ApiProperty({
+    description: 'The target language for the translation',
+    enum: Lang,
+    example: Lang.SPANISH,
+  })
   @IsEnum(Lang)
   readonly lang: Lang;
 }
