d4b6492a2f6c50ef56cb98f39e08e8b0972772e4
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-22 09:28:19 -0500

    docs(image-variation.dto.ts): add documentation with swagger
    add description and minLength properties

--- src/gpt/dtos/image/image-variation.dto.ts
+++ src/gpt/dtos/image/image-variation.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUrl, MinLength } from 'class-validator';
 
 export class ImageVariationDto {
+  @ApiProperty({
+    description: 'The base image URL',
+    minLength: 10,
+  })
   @IsString()
   @IsUrl()
   @MinLength(10)
